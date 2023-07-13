342143a8ce21d8d740b0c04d0beaf4d653c6257a
Brunão <sousabs205@gmail.com>
2023-07-13 11:36:30 -0300

    Atividade smartTV

---
+++ sistema-smart-tv/src/SmartTV.java
@@ -0,0 +1,37 @@
+public class SmartTV {
+    static boolean ligada = false;
+    static int canal = 1;
+    static int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+    
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume para" + volume);
+    }
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume para" + volume);
+    }
+
+    
+    public void mudarDeCanal(int novoCanal){
+        canal = novoCanal;
+        
+    }
+     
+
+    public void aumentarCanal(){
+        canal++;
+    }
+     
+    public void diminuirCanal(){
+        canal--;
+    }
+}
